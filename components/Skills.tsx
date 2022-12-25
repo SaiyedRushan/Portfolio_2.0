266684a83b3d684f4bd2c98664cf77f01a72acf3
SaiyedRushan <rushan52@gmail.com>
2022-12-24 19:49:53 -0500

    Added skills section

---
+++ components/Skills.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Skill from "./Skill";
+type Props = {};
+
+function Skills({}: Props) {
+  return (
+    <motion.div
+      className="flex flex-col relative text-center md:text-left xl:flex-row max-w-[2000px] xl:px-10 min-h-screen justify-center xl:space-y-0 mx-auto items-center"
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ duration: 1.5 }}
+    >
+      <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl ">
+        Skills
+      </h3>
+
+      <h3 className="absolute top-36 uppercase tracking-[3px] text-gray-500 text-sm">
+        Hover over a skill for currency proficiency
+      </h3>
+
+      <div className="grid grid-cols-4 gap-5">
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+        <Skill />
+      </div>
+    </motion.div>
+  );
+}
+
+export default Skills;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Hero from "../components/Hero";
 import About from "../components/About";
 import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,6 +29,10 @@ export default function Home() {
 
         <section id="experience" className="snap-center">
           <WorkExperience />
+        </section>
+
+        <section id="skills" className="snap-start">
+          <Skills />
         </section>
       </div>
     </>
