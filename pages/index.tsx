ad1c0eda2e79a3d5d5b78e68daf1b40e489b76f7
SaiyedRushan <rushan52@gmail.com>
2022-12-25 13:55:23 -0500

    added back to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,13 +7,14 @@ import WorkExperience from "../components/WorkExperience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflox-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
+      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflox-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scroll-smooth">
         <Head>
           <title>Rushan's Portfolio</title>
           <link rel="icon" href="/favicon.ico" />
@@ -44,6 +45,17 @@ export default function Home() {
         <section id="contact" className="snap-start">
           <ContactMe />
         </section>
+
+        <Link href="#hero" scroll={true}>
+          <footer className="sticky bottom-5 w-full cursor-pointer">
+            <div className="flex items-center justify-center">
+              <img
+                src="https://img.icons8.com/ios-filled/512/collapse-arrow.png"
+                className="h-10 w-10 rounded-full fliter grayscale hover:grayscale-0"
+              />
+            </div>
+          </footer>
+        </Link>
       </div>
     </>
   );
