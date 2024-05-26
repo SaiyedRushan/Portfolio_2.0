98a0f084ea6f583713b960a0a691220f597553f3
SaiyedRushan <rushan52@gmail.com>
2024-05-26 11:02:25 -0400

    fix missing head from _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document"
 export default function Document() {
   return (
     <Html lang='en'>
+      <Head />
       <body className='bg-[rgb(36,36,36)] text-white h-screen'>
         <Main />
         <NextScript />
