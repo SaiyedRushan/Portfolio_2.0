cee9eec4f5783137c8993b74ffa30df1e76cd43d
SaiyedRushan <rushan52@gmail.com>
2024-08-18 15:18:44 -0400

    blogs styling update - center images, update font

--- pages/blogs/[slug].tsx
+++ pages/blogs/[slug].tsx
@@ -10,7 +10,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   let post = await fetchEntry(params?.slug as string)
   const stringifiedData = safeJsonStringify(post!)
   post = JSON.parse(stringifiedData)
-
   return {
     props: {
       post: post!,
@@ -37,7 +36,11 @@ function renderEntry(node: any) {
   if (node.data.target.sys.contentType.sys.id === "videoEmbed") {
     return <iframe src={node.data.target.fields.embedUrl} height='100%' width='100%' title={node.data.target.fields.title} allowFullScreen={true} />
   } else if (node.data.target.sys.contentType.sys.id === "componentRichImage") {
-    return <img className='p-10' src={`https://${node.data.target.fields.image.fields?.file.url}`} height={node.data.target.fields.image.fields?.file.details.image.height} width={node.data.target.fields.image.fields?.file.details.image.width} alt={node.data.target.fields.image.fields.description} />
+    return (
+      <div className='flex justify-center'>
+        <img className='p-10' src={`https://${node.data.target.fields.image.fields?.file.url}`} height={node.data.target.fields.image.fields?.file.details.image.height} width={node.data.target.fields.image.fields?.file.details.image.width} alt={node.data.target.fields.image.fields.description} />
+      </div>
+    )
   }
 }
 
@@ -56,15 +59,16 @@ export default function Post({ post }: PostProps) {
   return (
     <>
       <Header />
-      <div className='flex flex-col gap-3 items-center'>
+      <div className='flex flex-col gap-3 items-center max-w-7xl mx-auto font-mono'>
         <Head>
           <title>{post.fields.title as string}</title>
         </Head>
-        <h1>{post.fields.title as string}</h1>
+        <h1 className='text-3xl'>{post.fields.title as string}</h1>
+        <h2>{post.fields.author.fields.name as string}</h2>
         <p suppressHydrationWarning>{new Date(post.fields.publishedDate as string).toLocaleDateString()}</p>
         <p>{post.fields.shortDescription as string}</p>
-        <img src={`https://${post.fields.featuredImage?.fields?.file?.url}`} />
-        <div className='mx-5 my-2' suppressHydrationWarning>
+        <img src={`https://${post.fields.featuredImage?.fields?.file?.url}`} className='h-[30vh]' />
+        <div className='mx-5 my-2 text-lg' suppressHydrationWarning>
           {documentToReactComponents(post.fields.content as any, renderOptions)}
         </div>
       </div>
