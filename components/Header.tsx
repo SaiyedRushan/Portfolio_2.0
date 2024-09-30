a7a353ad6e996850d14fdf7dc124f6ec831dadcc
Rushanshah Saiyed <rushan52@gmail.com>
2024-09-30 15:05:59 -0400

    add sound effects on button hover

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,31 +1,81 @@
-import React from "react"
-import { SocialIcon } from "react-social-icons"
-import { motion } from "framer-motion"
-import Link from "next/link"
-import { Home } from "@mui/icons-material"
+import React from "react";
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { Home } from "@mui/icons-material";
+import useSound from "use-sound";
 
 function Header() {
+  const [play] = useSound("mouse-click.wav", {
+    volume: 0.1,
+    playbackRate: 2.0,
+  });
   return (
-    <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center bg-transparent'>
-      <motion.div initial={{ x: -500, opacity: 0, scale: 0.5 }} animate={{ x: 0, opacity: 1, scale: 1 }} transition={{ duration: 1.0 }} className='flex flex-row items-center gap-3'>
-        <Link href='/' className='hover:shadow-lg hover:scale-125 transform transition-transform duration-300'>
-          <Home className='text-gray-400'></Home>
+    <header className="sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center bg-transparent">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.0 }}
+        className="flex flex-row items-center gap-3"
+      >
+        <Link
+          href="/"
+          className="hover:shadow-lg hover:scale-125 transform transition-transform duration-300"
+          onMouseEnter={() => play()}
+        >
+          <Home className="text-gray-400"></Home>
         </Link>
-        <SocialIcon url='https://www.linkedin.com/in/rushanshahsaiyed/' fgColor='gray' target='_blank' bgColor='transparent' className='hover:shadow-lg hover:scale-125 transform transition-transform duration-300'></SocialIcon>
-        <SocialIcon url='https://github.com/SaiyedRushan' target='_blank' fgColor='gray' bgColor='transparent' className='hover:shadow-lg hover:scale-125 transform transition-transform duration-300'></SocialIcon>
-        <Link href='/blogs' className='hover:shadow-lg hover:scale-125 transform transition-transform duration-300'>
-          <p className='uppercase text-sm text-gray-400 p-3'>Blog</p>
+        <SocialIcon
+          url="https://www.linkedin.com/in/rushanshahsaiyed/"
+          fgColor="gray"
+          target="_blank"
+          bgColor="transparent"
+          className="hover:shadow-lg hover:scale-125 transform transition-transform duration-300"
+          onMouseEnter={() => play()}
+        ></SocialIcon>
+        <SocialIcon
+          url="https://github.com/SaiyedRushan"
+          target="_blank"
+          fgColor="gray"
+          bgColor="transparent"
+          className="hover:shadow-lg hover:scale-125 transform transition-transform duration-300"
+          onMouseEnter={() => play()}
+        ></SocialIcon>
+        <Link
+          href="/blogs"
+          className="hover:shadow-lg hover:scale-125 transform transition-transform duration-300"
+          onMouseEnter={() => play()}
+        >
+          <p className="uppercase text-sm text-gray-400 p-3">Blog</p>
         </Link>
       </motion.div>
 
-      <motion.div initial={{ x: 500, opacity: 0, scale: 0.5 }} animate={{ x: 0, opacity: 1, scale: 1 }} transition={{ duration: 1.0 }} className='flex flex-row items-center text-gray-300 cursor-pointer'>
-        <SocialIcon network='email' fgColor='gray' bgColor='transparent' className='cursor-pointer hover:shadow-lg hover:scale-125 transform transition-transform duration-300' url='mailto:rushan52@gmail.com' />
-        <Link href='/#contact' className='hover:shadow-lg hover:scale-125 transform transition-transform duration-300 mx-4'>
-          <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get in touch</p>
+      <motion.div
+        initial={{ x: 500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        transition={{ duration: 1.0 }}
+        className="flex flex-row items-center text-gray-300 cursor-pointer"
+      >
+        <SocialIcon
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+          className="cursor-pointer hover:shadow-lg hover:scale-125 transform transition-transform duration-300"
+          url="mailto:rushan52@gmail.com"
+          onMouseEnter={() => play()}
+        />
+        <Link
+          href="/#contact"
+          onMouseEnter={() => play()}
+          className="hover:shadow-lg hover:scale-125 transform transition-transform duration-300 mx-4"
+        >
+          <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+            Get in touch
+          </p>
         </Link>
       </motion.div>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,52 @@
-import Hero from "../components/Hero"
-import About from "../components/About"
-import WorkExperience from "../components/WorkExperience"
-import Skills from "../components/Skills"
-import Projects from "../components/Projects"
-import ContactMe from "../components/ContactMe"
-import Link from "next/link"
-import Header from "../components/Header"
-import { ArrowUpCircleIcon } from "@heroicons/react/24/solid"
-import { Button } from "@chakra-ui/react"
-import { BackgroundBeams } from "../components/ui/background-beams"
+import Hero from "../components/Hero";
+import About from "../components/About";
+import WorkExperience from "../components/WorkExperience";
+import Skills from "../components/Skills";
+import Projects from "../components/Projects";
+import ContactMe from "../components/ContactMe";
+import Link from "next/link";
+import Header from "../components/Header";
+import { ArrowUpCircleIcon } from "@heroicons/react/24/solid";
+import { Button } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <>
-      <div className='bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflox-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#008000]/80 scroll-smooth'>
-        {/* <BackgroundBeams /> */}
+      <div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflox-x-hidden z-0 scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#008000]/80 scroll-smooth">
         <Header />
 
-        <section id='hero' className='snap-start'>
+        <section id="hero" className="snap-start">
           <Hero />
         </section>
 
-        <section id='about' className='snap-center'>
+        <section id="about" className="snap-center">
           <About />
         </section>
 
-        <section id='experience' className='snap-center'>
+        <section id="experience" className="snap-center">
           <WorkExperience />
         </section>
 
-        <section id='projects' className='snap-start'>
+        <section id="projects" className="snap-start">
           <Projects />
         </section>
 
-        <section id='skills' className='snap-start'>
+        <section id="skills" className="snap-start">
           <Skills />
         </section>
 
-        <section id='contact' className='snap-start'>
+        <section id="contact" className="snap-start">
           <ContactMe />
         </section>
 
-        <div className='fixed bottom-5 right-5 z-50'>
-          <Link href='#hero' scroll={true}>
-            <Button className='h-14 w-14 p-0 rounded-full '>
-              <ArrowUpCircleIcon className='h-10 w-10' fill='slate' />
+        <div className="fixed bottom-5 right-5 z-50">
+          <Link href="#hero" scroll={true}>
+            <Button className="h-14 w-14 p-0 rounded-full ">
+              <ArrowUpCircleIcon className="h-10 w-10" fill="slate" />
             </Button>
           </Link>
         </div>
       </div>
     </>
-  )
+  );
 }
